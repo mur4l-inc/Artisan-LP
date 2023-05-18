60b092cafc920a8d5ff792d5293d949c3c2963a3
Shinya Takahashi <shinya.takahashi@salesforce.com>
2023-05-18 22:51:09 +0900

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@
 import { IResult } from '@/interface';
 import Results from '@/components/Results';
 
-export const dynamic='force-dynamic';
+// export const dynamic='force-dynamic';
 const API_KEY = process.env.API_KEY
 
 export default async function Home() {
