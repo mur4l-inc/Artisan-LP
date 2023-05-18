b3d64773fc17dd9c06638e5686081ed857bbd441
Shinya Takahashi <shinya.takahashi@salesforce.com>
2023-05-18 22:49:49 +0900

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,14 @@ import Results from '@/components/Results';
 export const dynamic='force-dynamic';
 const API_KEY = process.env.API_KEY
 
-export default async function Home({ searchParams } : {searchParams?: any}) {
-  const genre = searchParams?.genre || 'fetchTrending';
+export default async function Home() {
   // const genre = 'fetchTopRated';
 
     // res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'
     //   ? 'movie/top_rated' :
     //     'trending/all/week'
     // }?api_key=${API_KEY}&language=en-US&page=1`, { next: {revalidate: 100000 }})
-  const res = await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'
-      ? 'movie/top_rated' :
-        'trending/all/week'
-    }?api_key=${API_KEY}&language=en-US&page=1`, { cache: 'no-store' })
+  const res = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`, { cache: 'no-store' })
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
